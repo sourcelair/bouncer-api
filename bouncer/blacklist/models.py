509065fa90f64ec10f8fccd482de850ae5fbe8a5
Nikolina Zisouli <nzisouli@gmail.com>
2018-07-19 11:45:07 +0300

    Created models IP_Entry, Email_Entry, Host_Entry

--- bouncer/blacklist/models.py
+++ bouncer/blacklist/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class IP_Entry(models.Model):
+	entry_value = models.GenericIPAddressField()
+
+class Email_Entry(models.Model):
+	entry_value = models.EmailField()
+
+class Host_Entry(models.Model):
+	entry_value = models.CharField(max_length = 254)
--- bouncer/bouncer/settings.py
+++ bouncer/bouncer/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blacklist.apps.BlacklistConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
