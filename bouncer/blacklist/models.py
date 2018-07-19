0130af3ab28662a1583ad3b83957e0f9a46f60be
Nikolina Zisouli <nzisouli@gmail.com>
2018-07-19 12:33:05 +0300

    Changed reason field to an abstract base class, added docstrings.

--- bouncer/blacklist/models.py
+++ bouncer/blacklist/models.py
@@ -1,14 +1,21 @@
 from django.db import models
 
+class EntryReason(models.Model):
+	'''Abstract base class for the reason why an entry is blacklisted.'''
+	reason = models.TextField()
+
+	class Meta:
+		abstract = True
+
 # Create your models here.
-class IPEntry(models.Model):
+class IPEntry(EntryReason):
+	'''Model for IP entries in blacklist.'''
 	entry_value = models.GenericIPAddressField()
-	reason = models.TextField()
 
-class EmailEntry(models.Model):
+class EmailEntry(EntryReason):
+	'''Model for email entries in blacklist.'''
 	entry_value = models.EmailField()
-	reason = models.TextField()
 
-class EmailHostEntry(models.Model):
+class EmailHostEntry(EntryReason):
+	'''Model for email host entries in blacklist.'''
 	entry_value = models.CharField(max_length = 254)
-	reason = models.TextField()
