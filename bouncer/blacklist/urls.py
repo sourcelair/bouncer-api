8dcf497dfc520dd23fa79ee6c73b0fb6771b8905
Nikolina Zisouli <nzisouli@gmail.com>
2018-07-30 12:45:38 +0300

    Add view to check if ip in get request is blacklisted

--- bouncer/blacklist/urls.py
+++ bouncer/blacklist/urls.py
@@ -3,7 +3,5 @@ from django.urls import path
 from . import views
 
 app_name = "blacklist"
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("?ip=<str:ip>", views.ip_request, name="ip request"),
-]
+
+urlpatterns = [path("", views.IPRequestView.as_view(), name="ip-request")]
