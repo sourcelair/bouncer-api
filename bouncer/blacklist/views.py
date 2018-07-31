1533915259cb0e2a6b10e74a0efcb8c2e4735898
Nikolina Zisouli <nzisouli@gmail.com>
2018-07-31 12:31:34 +0300

    Create python dict from request.GET and handle multiple queries on the same resource

--- bouncer/blacklist/views.py
+++ bouncer/blacklist/views.py
@@ -14,28 +14,34 @@ class IPRequestView(View):
     template = loader.get_template("blacklist/ip_request.html")
 
     def get(self, request, *args, **kwargs):
+        query = dict(self.request.GET.lists())
         context = {"request_list": []}
-        if "email" in self.request.GET:
-            context["request_list"].append("email")
-            context["request_list"].append(self.request.GET["email"])
-            if is_email_blacklisted(self.request.GET["email"]):
-                context["request_list"].append("YES")
-            else:
-                context["request_list"].append("NO")
-        if "email_host" in self.request.GET:
-            context["request_list"].append("email_host")
-            context["request_list"].append(self.request.GET["email_host"])
-            if is_email_host_blacklisted(self.request.GET["email_host"]):
-                context["request_list"].append("YES")
-            else:
-                context["request_list"].append("NO")
-        if "ip" in self.request.GET:
-            context["request_list"].append("ip")
-            context["request_list"].append(self.request.GET["ip"])
-            if is_ip_blacklisted(self.request.GET["ip"]):
-                context["request_list"].append("YES")
-            else:
-                context["request_list"].append("NO")
+        if "email" in query:
+            while query["email"] != []:
+                context["request_list"].append("email")
+                context["request_list"].append(query["email"][0])
+                if is_email_blacklisted(query["email"][0]):
+                    context["request_list"].append("YES")
+                else:
+                    context["request_list"].append("NO")
+                query["email"].remove(query["email"][0])
+        if "email_host" in query:
+            while query["email_host"] != []:
+                context["request_list"].append("email_host")
+                context["request_list"].append(query["email_host"][0])
+                if is_email_host_blacklisted(query["email_host"][0]):
+                    context["request_list"].append("YES")
+                else:
+                    context["request_list"].append("NO")
+                query["email_host"].remove(query["email_host"][0])
+        if "ip" in query:
+            while query["ip"] != []:
+                context["request_list"].append("ip")
+                context["request_list"].append(query["ip"][0])
+                if is_ip_blacklisted(query["ip"][0]):
+                    context["request_list"].append("YES")
+                else:
+                    context["request_list"].append("NO")
+                query["ip"].remove(query["ip"][0])
 
-        # print (context)
         return HttpResponse(self.template.render(context, request))
