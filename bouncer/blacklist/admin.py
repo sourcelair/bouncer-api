8079231c57b771ea7f59feabfe684b97af0dbdce
Nikolina Zisouli <nzisouli@gmail.com>
2018-07-31 17:17:04 +0300

    Remove lower_case_entry_value from admin

--- bouncer/blacklist/admin.py
+++ bouncer/blacklist/admin.py
@@ -4,12 +4,12 @@ from blacklist import models
 
 @admin.register(models.IPEntry, models.EmailHostEntry)
 class EntryAdmin(admin.ModelAdmin):
-    fields = ["entry_value", "lower_case_entry_value", "reason"]
+    fields = ["entry_value", "reason"]
     search_fields = ["entry_value"]
 
 
 @admin.register(models.EmailEntry)
 class EmailEntryAdmin(admin.ModelAdmin):
-    fields = ["entry_value", "lower_case_entry_value", "hashed_value", "reason"]
+    fields = ["entry_value", "hashed_value", "reason"]
     search_fields = ["entry_value", "hashed_value"]
     readonly_fields = ["hashed_value"]
