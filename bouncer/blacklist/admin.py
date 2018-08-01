a1043809c18a206d8817e0dbc4442172841e336c
Nikolina Zisouli <nzisouli@gmail.com>
2018-08-01 11:48:28 +0300

    Make lower_case_entry_value non-editable and remove it from admin

--- bouncer/blacklist/admin.py
+++ bouncer/blacklist/admin.py
@@ -4,6 +4,5 @@ from blacklist import models
 
 @admin.register(models.IPEntry, models.EmailEntry, models.EmailHostEntry)
 class EntryAdmin(admin.ModelAdmin):
-    fields = ["entry_value", "lower_case_entry_value", "reason"]
-    search_fields = ["lower_case_entry_value", "entry_value"]
-    readonly_fields = ["lower_case_entry_value"]
+    fields = ["entry_value", "reason"]
+    search_fields = ["entry_value"]
