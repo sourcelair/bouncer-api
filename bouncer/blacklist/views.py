7ffacf6e7a526a49a7c687eb2ab2dd51a63ea090
Nikolina Zisouli <nzisouli@gmail.com>
2018-08-02 15:24:42 +0300

    Use .getlist() to get queries

--- bouncer/blacklist/views.py
+++ bouncer/blacklist/views.py
@@ -15,29 +15,28 @@ class RequestView(View):
     template = loader.get_template("blacklist/request.html")
 
     def get(self, request, *args, **kwargs):
-        query = namedtuple("query", self.request.GET.keys())(**self.request.GET)
         context = {"results": []}
-        Row = namedtuple('Row', ['kind', 'value', 'result'])
-        if "email" in self.request.GET:
-            for email in query.email:
-                result = "NO"
-                if is_email_blacklisted(email):
-                    result = "YES"
-                row = Row('email', email, result)
-                context["results"].append(row)
-        if "email_host" in self.request.GET:
-            for host in query.email_host:
-                result = "NO"
-                if is_email_host_blacklisted(host):
-                    result = "YES"
-                row = Row('email_host', host, result)
-                context["results"].append(row)
-        if "ip" in self.request.GET:
-            for ip in query.ip:
-                result = "NO"
-                if is_ip_blacklisted(ip):
-                    result = "YES"
-                row = Row('ip', ip, result)
-                context["results"].append(row)
+        Row = namedtuple("Row", ["kind", "value", "result"])
+        email_query = self.request.GET.getlist("email")
+        for email in email_query:
+            result = "NO"
+            if is_email_blacklisted(email):
+                result = "YES"
+            row = Row("email", email, result)
+            context["results"].append(row)
+        host_query = self.request.GET.getlist("email_host")
+        for host in host_query:
+            result = "NO"
+            if is_email_host_blacklisted(host):
+                result = "YES"
+            row = Row("email_host", host, result)
+            context["results"].append(row)
+        ip_query = self.request.GET.getlist("ip")
+        for ip in ip_query:
+            result = "NO"
+            if is_ip_blacklisted(ip):
+                result = "YES"
+            row = Row("ip", ip, result)
+            context["results"].append(row)
 
         return HttpResponse(self.template.render(context, request))
