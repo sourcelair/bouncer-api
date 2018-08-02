18906872b0821612c4c52550b4d9bba947b524a9
Nikolina Zisouli <nzisouli@gmail.com>
2018-08-02 11:41:48 +0300

    Use .exists() when checking for entry existence

--- bouncer/blacklist/brain.py
+++ bouncer/blacklist/brain.py
@@ -6,9 +6,7 @@ def is_ip_blacklisted(ip):
     Checks if ip is in IPEntry model.
     """
 
-    if models.IPEntry.objects.filter(lower_case_entry_value=ip.lower()):
-        return True
-    return False
+    return models.IPEntry.objects.filter(lower_case_entry_value=ip.lower()).exists()
 
 
 def is_email_blacklisted(email):
@@ -16,7 +14,7 @@ def is_email_blacklisted(email):
     Checks if email is in EmailEntry model or host is in EmailHostEntry model.
     """
 
-    if models.EmailEntry.objects.filter(lower_case_entry_value=email.lower()):
+    if models.EmailEntry.objects.filter(lower_case_entry_value=email.lower()).exists():
         return True
     host = email.split("@")[1]
     return is_email_host_blacklisted(host)
@@ -27,9 +25,9 @@ def is_email_host_blacklisted(host):
     Checks if email host is in EmailHostEntry model.
     """
 
-    if models.EmailHostEntry.objects.filter(lower_case_entry_value=host.lower()):
-        return True
-    return False
+    return models.EmailHostEntry.objects.filter(
+        lower_case_entry_value=host.lower()
+    ).exists()
 
 
 def is_email_hash_blacklisted(email_hash):
@@ -37,6 +35,4 @@ def is_email_hash_blacklisted(email_hash):
     Checks if email hashed value is in EmailEntry model
     """
 
-    if models.EmailEntry.objects.filter(hashed_value=email_hash.lower()):
-        return True
-    return False
+    return models.EmailEntry.objects.filter(hashed_value=email_hash.lower()).exists()
