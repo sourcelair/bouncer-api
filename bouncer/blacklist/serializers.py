98c81359e574e76e2cd071bae9954b8173ffd3ff
Nikolina Zisouli <nzisouli@gmail.com>
2018-08-06 12:10:03 +0300

    Format with black

--- bouncer/blacklist/serializers.py
+++ bouncer/blacklist/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from blacklist import models
 
+
 class ResponseSerializer(serializers.Serializer):
     kind = serializers.CharField()
     value = serializers.CharField()
