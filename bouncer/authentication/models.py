8ae7583c9cb93e4b27dc7ca77436fe5ef3ff2b49
Nikolina Zisouli <nzisouli@gmail.com>
2018-08-20 12:42:05 +0300

    Remove _("Key") and class Meta from AuthToken

--- bouncer/authentication/models.py
+++ bouncer/authentication/models.py
@@ -9,16 +9,12 @@ class AuthToken(models.Model):
         return get_random_string(length=32)
 
     key = models.CharField(
-        _("Key"), max_length=32, default=generate_key, primary_key=True, editable=False
+       max_length=32, default=generate_key, primary_key=True, editable=False
     )
     user = models.ManyToManyField(
         settings.AUTH_USER_MODEL, related_name="auth_token", verbose_name=_("User")
     )
     created_at = models.DateTimeField(_("Created"), auto_now_add=True)
-
-    class Meta:
-        verbose_name = _("AuthToken")
-        verbose_name_plural = _("AuthTokens")
 
     def __str__(self):
         return self.key
