b8cd6f69244651786d59c5c580ea67af9ec989e1
Nikolina Zisouli <nzisouli@gmail.com>
2018-08-30 11:48:46 +0300

    Merge permissions into one class

--- bouncer/bouncer/settings.py
+++ bouncer/bouncer/settings.py
@@ -50,8 +50,7 @@ REST_FRAMEWORK = {
     ),
     "DEFAULT_PERMISSION_CLASSES": (
         "rest_framework.permissions.IsAuthenticated",
-        "authentication.permissions.GetRequestViewPermission",
-        "authentication.permissions.PostRequestViewSetPermission",
+        "authentication.permissions.RequestPermission",
     ),
 }
 
